c82fc5309fac2008ffe079ecd2f160469157d644
dongguabai <guabaidong@gmail.com>
2024-01-25 18:42:06 +0800

    feat:ImageCompressor

--- src/main/java/com/github/dongguabai/tools/image/ImageCompressor.java
+++ src/main/java/com/github/dongguabai/tools/image/ImageCompressor.java
@@ -15,33 +15,34 @@ import java.util.List;
  * @author dongguabai
  * @date 2024-01-25 18:18
  */
+
 public class ImageCompressor extends JFrame {
     private JSlider slider;
 
     public ImageCompressor() {
         super("Image Compressor");
 
-        slider = new JSlider(1, 100, 50);
+        slider = new JSlider(0, 100, 50);
         slider.setMajorTickSpacing(10);
         slider.setPaintTicks(true);
         slider.setPaintLabels(true);
 
         new DropTarget(this, new DropTargetAdapter() {
-            @Override
             public void drop(DropTargetDropEvent dtde) {
                 try {
                     dtde.acceptDrop(dtde.getDropAction());
                     if (dtde.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
                         List<File> files = (List<File>) dtde.getTransferable().getTransferData(DataFlavor.javaFileListFlavor);
                         for (File file : files) {
+                            String extension = getFileExtension(file);
                             BufferedImage image = ImageIO.read(file);
                             double scale = slider.getValue() / 100.0;
                             BufferedImage compressedImage = compressImage(image, scale);
-                            File output = new File(file.getParent(), System.currentTimeMillis() + "compress.png");
-                            ImageIO.write(compressedImage, "png", output);
+                            File output = new File(file.getParent(), System.currentTimeMillis() + "compress." + extension);
+                            ImageIO.write(compressedImage, extension, output);
                         }
                     } else {
-                        // todo 处理不支持的 DataFlavor
+                        // 处理不支持的 DataFlavor
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -50,9 +51,10 @@ public class ImageCompressor extends JFrame {
         });
 
         add(slider, BorderLayout.NORTH);
-        setSize(400, 300);
+        setSize(800, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        setVisible(true);
     }
 
     public static BufferedImage compressImage(BufferedImage sourceImage, double scale) {
@@ -64,6 +66,15 @@ public class ImageCompressor extends JFrame {
         g.drawImage(sourceImage, 0, 0, width, height, 0, 0, sourceImage.getWidth(), sourceImage.getHeight(), null);
         g.dispose();
         return resized;
+    }
+
+    public static String getFileExtension(File file) throws Exception {
+        String fileName = file.getName();
+        int dotIndex = fileName.lastIndexOf('.');
+        if (dotIndex == -1) {
+            throw new Exception("No extension found for file: " + fileName);
+        }
+        return fileName.substring(dotIndex + 1);
     }
 
     public static void main(String[] args) {
